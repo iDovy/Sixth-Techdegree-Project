310f64ad6bb0652a977f69d798ad730808f65437
iDovy <dovydas.simonovas@gmail.com>
2018-02-11 17:52:36 +0200

    Finished the project
    Finished the project

--- video_player_v3/js/scripts.js
+++ video_player_v3/js/scripts.js
@@ -1,1 +1,21 @@
-import "mediaelement-mediaelement-85f4484/full.js";
+const vid = document.querySelector("video");
+const spans = document.querySelectorAll("span");
+const text = document.querySelector(".text")
+
+vid.addEventListener("timeupdate", () => {
+    var time = vid.currentTime;
+    for (let i = 0; i < spans.length; i++) {
+        if (spans[i].getAttribute("data-start") <= time && spans[i].getAttribute("data-end") >= time )
+        {
+            spans[i].style.color = "#E8B63B";
+        }
+        else{
+            spans[i].style.color = "#000";
+        }
+    }
+});
+text.addEventListener('click', (e) => {
+    if(e.target.tagName == "SPAN"){
+        vid.currentTime = e.target.getAttribute("data-start");
+    }
+});
